4ea292c897ffdd770cd27b32ceab19d1c7fe0693
DieterR97 <221122@virtualwindow.co.za>
2024-03-16 05:28:45 +0200

    Mostly Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import './index.css';
 import HeroImage2 from './components/HeroImage2';
 import Logo from './images/logo.png';
 import mars from './images/Mars.png';
@@ -9,7 +10,10 @@ import saturn from './images/Saturn.png';
 import mobileBG from './images/mobile_app_background.jfif';
 import astronaut from './images/astronaut.png';
 import iPhone15 from './images/iPhone15.png';
-
+import instagram from './images/instagram.png';
+import tiktok from './images/tiktok.png';
+import facebook from './images/facebook.png';
+import twitter from './images/twitter.png';
 function App() {
 
   const [activeLink, setActiveLink] = useState('link1');
@@ -58,11 +62,11 @@ function App() {
             <div class="collapse navbar-collapse navbar-container" id="navbarText">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className={activeLink === 'link1' ? 'nav-link active-link' : 'nav-link'} onClick={() => handleLinkClick('link1')}>
-                  <a href="#start">Home</a>
+                  <a href="#start" className='navLink'>Home</a>
                   <div class="underline"></div>
                 </li>
                 <li className={activeLink === 'link2' ? 'nav-link active-link' : 'nav-link'} onClick={() => handleLinkClick('link2')}>
-                  <a href="#mid">Destinations</a>
+                  <a href="#mid" className='navLink'>Destinations</a>
                   <div class="underline"></div>
                 </li>
               </ul>
@@ -70,7 +74,7 @@ function App() {
 
           </div>
 
-          <button type="button" class="btn btn-primary btn-lg" onClick={scrollToElement}>Large button</button>
+          <button type="button" class="btn-get-app" onClick={scrollToElement}>Get the app</button>
 
         </div>
       </nav>
@@ -87,7 +91,7 @@ function App() {
         Explore three breathtaking destinations, each with its own unique charm and mystery.
       </h5>
 
-      <div className='container destC'>
+      <div className='container-fluid destC'>
         <div className='row'>
 
           <div className='col-1'>
@@ -96,10 +100,33 @@ function App() {
 
           <div className='col-10'>
 
+            {/* <div className='container-fluid'>
+            </div> */}
             <div class="row">
 
-              <div class="col-4 destCard">
-                <div class="card h-100">
+              <div class="col-4 d-flex justify-content-center">
+                <div class="card w-100">
+
+                  <div class="card-body">
+                    <img src={mars} className='planet-img'></img>
+                  </div>
+
+                  <div class="card-footer d-flex flex-column justify-content-between">
+                    <div className='destination-heading'>
+                      Mars
+                    </div>
+                    <div className='seperation-container d-flex flex-column justify-content-center text-center'>
+                      <div className='seperation-line'>
+                      </div>
+                    </div>
+                    <small class="text-body-secondary w-100">Engage in thrilling activities like rover expeditions and marvel at the breathtaking Martian sunsets. Say hey to Elon!</small>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="col-4 d-flex justify-content-center">
+                <div class="card w-100">
 
                   <div class="card-body">
                     <img src={mars} className='planet-img'></img>
@@ -119,29 +146,8 @@ function App() {
                 </div>
               </div>
 
-              <div class="col-4 destCard">
-                <div class="card h-100">
-
-                  <div class="card-body">
-                    <img src={mars} className='planet-img'></img>
-                  </div>
-
-                  <div class="card-footer">
-                    <small class="text-body-secondary">Engage in thrilling activities like rover expeditions and marvel at the breathtaking Martian sunsets. Say hey to Elon!</small>
-                    <div className='seperation-container d-flex flex-column justify-content-center text-center'>
-                      <div className='seperation-line'>
-                      </div>
-                    </div>
-                    <div className='destination-heading'>
-                      Mars
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-
-              <div class="col-4 destCard">
-                <div class="card h-100">
+              <div class="col-4 d-flex justify-content-center">
+                <div class="card w-100">
 
                   <div class="card-body">
                     <img src={mars} className='planet-img'></img>
@@ -163,27 +169,100 @@ function App() {
 
             </div>
           </div>
-          <div className='col-1'></div>
+
+          <div className='col-1'>
+
+          </div>
+
         </div>
       </div>
 
       {/* ==========================MOBILE APP BG IMAGE========================== */}
-      <div
-        className='text-center bg-image2 content'
-        style={{ backgroundImage: `url(${mobileBG})` }}
-        id='end'
-      >
+      <div class="ccontainer">
+
+        <div
+          className='text-center bg-image2 content iimage'
+          style={{ backgroundImage: `url(${mobileBG})` }}
+          id='end'
+        >
+        </div>
+
+        <div class="ttext">
+
+          <h3 className='newSec lt'>
+            THE STARS IN YOUR POCKET... Coming soon
+          </h3>
+
+          <h5 className='newSec2 lt2'>
+            We are very excited to launch our very own Celestial Voyages mobile application coming soon to the App Store & Google Play Store.
+          </h5>
+
+        </div>
+
+        <img src={iPhone15} className='iphone'></img>
+
+        <img src={astronaut} className='astronaut'></img>
+
+
       </div>
 
+      {/* ==========================FOOTER========================== */}
+      <div className='footer'>
+
+        <div className='container-fluid subscribe-container'>
+          <div className='subscribe'>
+            <h3 className='subtext'>Subscribe to stay updated</h3>
+            <div className='inputrow'>
+              <input className='inputField' type='text' name='name' placeholder='Space traveller name'></input>
+              <input className='inputField' type='text' name='email' placeholder='Contact email address'></input>
+              <button type="button" class="btn btn-primary btn-lg newsBTN">Join newsletter</button>
+            </div>
+          </div>
+        </div>
 
 
 
+        <div className='container-fluid last-info'>
+          <div className='row'>
+            <div className='col-1'></div>
+            <div className='col-2 reg-box'>
+              <img src={Logo} className='small-logo'></img>
+              <div className='d-flex flex-row'>
+                <p className='author-text'>Dieter &nbsp;</p>
+                <p className='font-weight-bold author-text'>©&nbsp;&nbsp; 2024</p>
+              </div>
+            </div>
+            <div className='col-5'></div>
+            <div className='col-2'>
+              <ul className='links-footer'>
+                <span className='font-weight-bold'>Links</span>
+                <li><a href='#start'>Home</a></li>
+                <li><a href='#mid'>Destinations</a></li>
+                <li><a href='#end'>Mobile App</a></li>
+              </ul>
+            </div>
+            <div className='col-1 social-links'>
+              <p className='social-text font-weight-bold'>Follow Us</p>
+              <div>
+                <a href='https://www.instagram.com/' target="_blank"><img src={instagram} className='social-icon'></img></a>
+                <a href='https://www.tiktok.com/en/' target="_blank"><img src={tiktok} className='social-icon'></img></a>
+              </div>
+              <div>
+                <a href='https://www.facebook.com/' target="_blank"><img src={facebook} className='social-icon'></img></a>
+                <a href='https://twitter.com/?lang=en' target="_blank"><img src={twitter} className='social-icon'></img></a>
+              </div>
+            </div>
+            <div className='col-1'></div>
+          </div>
+        </div>
+
+
+      </div>
 
 
     </>
   );
 
 }
-
 
 export default App;
--- src/components/HeroImage2.jsx
+++ src/components/HeroImage2.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import heroImg from "../images/Header_background.jfif";
 import '../App.css';
+import '../index.css';
 
 export default function HeroImage2() {
+
+    const scrollToElement = () => {
+        const element = document.getElementById('end');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    const scrollToElementM = () => {
+        const element = document.getElementById('mid');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <>
             {/* ==========================HERO IMAGE & HERO TEXT========================== */}
@@ -11,16 +27,16 @@ export default function HeroImage2() {
                     className='p-5 text-center bg-image content'
                     style={{ backgroundImage: `url(${heroImg})` }}
                 >
-                    <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+                    <div className='mask'>
                         <div className='d-flex justify-content-center align-items-center h-100'>
                             <div className='text-white'>
-                                <h4>Tired Of Global Warming?</h4>
-                                <h1>Visit A
+                                <h4 className='q'>Tired Of Global Warming?</h4>
+                                <h1 className='cta'>Visit A
                                     Different Planet</h1>
-                                <p>Welcome to Celestial Voyages, where your dreams of interplanetary exploration come to life. Join us in venturing beyond the stars, discovering the wonders of otherworldly landscapes.</p>
-                                <div>
-                                    <button type="button" class="btn btn-primary btn-lg">Destinations</button>
-                                    <button type="button" class="btn btn-primary btn-lg">Stay up-to date</button>
+                                <p className='welcome'>Welcome to Celestial Voyages, where your dreams of interplanetary exploration come to life. Join us in venturing beyond the stars, discovering the wonders of otherworldly landscapes.</p>
+                                <div className='d-flex flex-row justify-content-center text-center'>
+                                    <button type="button" class="destination-btn" onClick={scrollToElementM}>Destinations</button>
+                                    <button type="button" class="stay-btn" onClick={scrollToElement}>Stay up-to date</button>
                                 </div>
                             </div>
                         </div>
